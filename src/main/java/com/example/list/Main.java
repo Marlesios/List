81a6591ef5a46af2dfb1fb63db73025266a6b71c
Marlesios <marles.nuralyev@gmail.com>
2022-10-17 11:32:21 +0300

    files uploaded

---
+++ src/main/java/com/example/list/Main.java
@@ -0,0 +1,34 @@
+package com.example.list;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println(" 1. Добавить задачу \n 2. вывести список задач \n 3. удалить задачу\n" +
+                    " 0. выход");
+            String input = scan.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+                switch (input) {
+                    case "1" -> {
+                        System.out.println("Введите задачу для планирования:");
+                        list.add(scan.nextLine());
+                    }
+                    case "2" -> {
+                        for (int i = 0; i < list.size(); i++) {
+                            System.out.printf("%s. %s \n", i + 1, list.get(i));
+                        }
+                    }
+                    case "3" -> {
+                        System.out.println("Введите номер задачи которую хотите удалить");
+                        list.remove(scan.nextInt() - 1);
+                    }
+
+            }
+        }
+    }
+}
